4e5c381753a1c36a928f46e656d5b0deae8ccab6
grayben <gray.ben.m@gmail.com>
2014-05-10 16:26:50 +1000

    Added "" default db values, allowed params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+	def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :first_name, :last_name, :profile_name, :password, :password_confirm) }
+  end
+
   protect_from_forgery with: :exception
 end
