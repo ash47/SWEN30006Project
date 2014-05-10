6fbfaf1a90ce47386f71bafb4af126913b9b24db
grayben <gray.ben.m@gmail.com>
2014-05-10 16:42:31 +1000

    Add nullcheck to user.{name, uni_reg, confirmed?}

--- db/migrate/20140510063744_create_clubs.rb
+++ db/migrate/20140510063744_create_clubs.rb
@@ -1,11 +1,11 @@
 class CreateClubs < ActiveRecord::Migration
   def change
     create_table :clubs do |t|
-      t.string :name
+      t.string :name, null: false, default: ""
       t.text :description
       t.string :website
-      t.integer :uni_registration_id
-      t.boolean :is_confirmed
+      t.integer :uni_registration_id, null: false, default: -1
+      t.boolean :is_confirmed, null: false, default: false
 
       t.timestamps
     end
