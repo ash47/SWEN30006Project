8359a147ee0590f637c6bdad2d36782b01f5045f
grayben <gray.ben.m@gmail.com>
2014-05-10 17:44:38 +1000

    Add navbar, reminder to test user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def flash_class type
+		case type
+		when :alert
+			return "alert-error"
+		when :notice
+			return "alert-notice"
+		else
+			""
+		end
+	end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  #Capitalise names
+
   # test "the truth" do
   #   assert true
   # end
