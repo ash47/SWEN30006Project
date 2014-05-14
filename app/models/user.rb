0f51070dea1fac7074b1bfaccc1902d2582a1150
grayben <gray.ben.m@gmail.com>
2014-05-14 17:45:41 +1000

    Validate fields on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: :true
+  validates :second_name, presence: :true
+  validates :email, presence: :true,
+  							uniqueness: true,
+  							format: {
+  								with: /\A^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$\z/, # \A and \z are "safe" anchors
+  								message: "must must be a valid email address"
+  							}
   def full_name
   	return first_name + " " + last_name  	
   end
