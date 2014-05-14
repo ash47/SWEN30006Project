ce0f1002ce588b8be2c1be7606b9e1cd060f4e30
grayben <gray.ben.m@gmail.com>
2014-05-14 17:52:24 +1000

    Correct user model validation name, update TODO

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, presence: :true
-  validates :second_name, presence: :true
+  validates :last_name, presence: :true
   validates :email, presence: :true,
   							uniqueness: true,
   							format: {
