edaeeb0c1d0a30f32940e4ed142170d570bc8129
grayben <gray.ben.m@gmail.com>
2014-05-25 14:40:54 +1000

    Confirmation emails configured

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525035202) do
+ActiveRecord::Schema.define(version: 20140525042513) do
 
   create_table "clubs", force: true do |t|
     t.string   "name",                default: "",    null: false
@@ -71,8 +71,13 @@ ActiveRecord::Schema.define(version: 20140525035202) do
     t.string   "last_name",              default: "",    null: false
     t.integer  "phone"
     t.boolean  "admin",                  default: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
